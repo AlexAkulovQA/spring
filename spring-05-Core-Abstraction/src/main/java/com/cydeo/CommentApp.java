269416519cbc6ee8d6b64d7c4dca5c6e4502b9a1
jamaldemir <jamal@cybertekschool.com>
2023-11-03 21:44:35 -0400

    spring-05-Core-Abstraction

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
@@ -0,0 +1,24 @@
+package com.cydeo;
+
+import com.cydeo.config.CommentConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CommentApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+        Comment comment = new Comment();
+        comment.setAuthor("Mike");
+        comment.setText("Spring Framework");
+
+        CommentService commentService = container.getBean(CommentService.class);
+
+        commentService.publishComment(comment);
+
+    }
+}
